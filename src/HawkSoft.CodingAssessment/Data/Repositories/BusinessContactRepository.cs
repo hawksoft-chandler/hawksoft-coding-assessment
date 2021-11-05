422f0f922de41c8ca7350fc3c82b157b3d0a7d58
Chandler Turner <development@chandlerturner.com>
2021-11-05 14:31:39 -0500

    Create BusinessContactRepository with Test Methods

---
+++ src/HawkSoft.CodingAssessment/Data/Repositories/BusinessContactRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HawkSoft.CodingAssessment.Data.Entities;
+using MongoDB.Driver;
+
+namespace HawkSoft.CodingAssessment.Data.Repositories
+{
+    public interface IBusinessContactRepository
+    {
+        Task<IEnumerable<UserBusinessContactDataEntity>> Get();
+    }
+
+    public class BusinessContactRepository : IBusinessContactRepository
+    {
+        private readonly IUserContactMongoDataContext _dataContext;
+
+        public BusinessContactRepository(IUserContactMongoDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IEnumerable<UserBusinessContactDataEntity>> Get()
+        {
+            return await _dataContext.BusinessContacts.Find(x => true).ToListAsync();
+        }
+    }
+}
