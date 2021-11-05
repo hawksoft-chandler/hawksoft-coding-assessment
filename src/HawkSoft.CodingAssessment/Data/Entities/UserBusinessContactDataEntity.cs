b9c945bf66950751ad1026ad45125ccfe4197cf5
Chandler Turner <development@chandlerturner.com>
2021-11-05 14:19:55 -0500

    Add Database Context and Entity Model

---
+++ src/HawkSoft.CodingAssessment/Data/Entities/UserBusinessContactDataEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace HawkSoft.CodingAssessment.Data.Entities
+{
+    public class UserBusinessContactDataEntity
+    {
+        [BsonId] public string Id { get; set; }
+
+        public IEnumerable<BusinessContactDataEntity> Contacts { get; set; }
+    }
+
+    public class BusinessContactDataEntity
+    {
+        [BsonId] public string Id { get; set; }
+
+        [BsonElement("email")] public string EmailAddress { get; set; }
+
+        [BsonElement("name")] public string Name { get; set; }
+
+        [BsonElement("address")] public string Address { get; set; }
+    }
+}
---
+++ src/HawkSoft.CodingAssessment/Data/UserContactMongoDataContext.cs
@@ -0,0 +1,24 @@
+﻿using HawkSoft.CodingAssessment.Data.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace HawkSoft.CodingAssessment.Data
+{
+    public interface IUserContactMongoDataContext
+    {
+        IMongoCollection<UserBusinessContactDataEntity> BusinessContacts { get; }
+    }
+
+    public class UserContactMongoDataContext : IUserContactMongoDataContext
+    {
+        public UserContactMongoDataContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("UserContactMongo"));
+            var database = client.GetDatabase("user-contacts");
+
+            BusinessContacts = database.GetCollection<UserBusinessContactDataEntity>("user-business-contacts");
+        }
+
+        public IMongoCollection<UserBusinessContactDataEntity> BusinessContacts { get; }
+    }
+}
