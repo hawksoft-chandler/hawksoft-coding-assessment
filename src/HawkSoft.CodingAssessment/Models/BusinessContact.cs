1f0d7161ec28dc523cbb7739be69e1d32cee89fa
Chandler Turner <development@chandlerturner.com>
2021-11-05 09:11:13 -0500

    Create BusinessContact Model

---
+++ src/HawkSoft.CodingAssessment/Models/BusinessContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawkSoft.CodingAssessment.Models
+{
+    public class BusinessContact
+    {
+        public string Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
