1dcaecc32e6454ea333185d147e7e7f0c03e8b07
Chandler Turner <development@chandlerturner.com>
2021-11-05 09:10:53 -0500

    Implement Results Infrastructure

---
+++ src/HawkSoft.CodingAssessment.Api/Services/ResultNotaryService.cs
@@ -0,0 +1,61 @@
+﻿using HawkSoft.CodingAssessment.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HawkSoft.CodingAssessment.Api.Services
+{
+    public interface IResultNotaryService
+    {
+        IActionResult NotarizeResult(IResult result);
+        IActionResult NotarizeResult<T>(IResult<T> result);
+    }
+
+    public class ResultNotaryService : IResultNotaryService
+    {
+        public IActionResult NotarizeResult<T>(IResult<T> result)
+        {
+            IActionResult output;
+            if (result.Success)
+            {
+                output = new OkObjectResult(result.Value);
+            }
+            else
+            {
+                if (result.IsException)
+                {
+                    output = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+                else
+                {
+                    output = new BadRequestResult();
+                }
+            }
+            return output;
+        }
+
+        public IActionResult NotarizeResult(IResult result)
+        {
+            IActionResult output;
+            if (result.Success)
+            {
+                output = new OkResult();
+            }
+            else
+            {
+                if (result.IsException)
+                {
+                    output = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+                else
+                {
+                    output = new BadRequestResult();
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ src/HawkSoft.CodingAssessment/Common/Result.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawkSoft.CodingAssessment.Common
+{
+    public interface IResult
+    {
+        bool Success { get; }
+        IEnumerable<string> FailureMessages { get; }
+        Exception Exception { get; }
+
+        bool IsException { get; }
+    }
+
+    public interface IResult<T> : IResult
+    {
+        T Value { get; }
+    }
+
+    public class Result : IResult
+    {
+        public bool Success { get; protected set; }
+
+        public IEnumerable<string> FailureMessages { get; protected set; }
+
+        public Exception Exception { get; protected set; }
+
+        public bool IsException => Exception != null;
+
+        protected Result()
+        {
+            Success = true;
+        }
+
+        protected Result(IEnumerable<string> messages)
+        {
+            Success = false;
+            FailureMessages = messages;
+        }
+
+        protected Result(Exception exception)
+        {
+            Success = false;
+            Exception = exception;
+        }
+
+        public static Result SuccessResult() => new Result();
+        public static Result FailureResult(IEnumerable<string> messages) => new Result(messages);
+        public static Result FailureResult(string message) => new Result(new[] { message });
+        public static Result ExceptionResult(Exception exception) => new Result(exception);
+    }
+
+    public class Result<T> : IResult<T>
+    {
+        public bool Success { get; protected set; }
+
+        public IEnumerable<string> FailureMessages { get; protected set; }
+
+        public Exception Exception { get; protected set; }
+
+        public bool IsException => Exception != null;
+        public T Value { get; protected set; }
+
+        protected Result(T value)
+        {
+            Value = value;
+            Success = true;
+        }
+        protected Result(IEnumerable<string> messages)
+        {
+            Success = false;
+            FailureMessages = messages;
+        }
+
+        protected Result(Exception exception)
+        {
+            Success = false;
+            Exception = exception;
+        }
+
+        public static Result<T> SuccessResult(T value) => new Result<T>(value);
+        public static Result<T> FailureResult(IEnumerable<string> messages) => new Result<T>(messages);
+        public static Result<T> FailureResult(string message) => new Result<T>(new[] { message });
+        public static Result<T> ExceptionResult(Exception exception) => new Result<T>(exception);
+
+    }
+}
